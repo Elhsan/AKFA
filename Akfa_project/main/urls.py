06d280daa00413c4ceb72c94e07689b90ca8e2fe
Elshan <elshanaliev2009@gmail.com>
2023-10-25 17:01:05 +0500

    elshan push

--- Akfa_project/main/urls.py
+++ Akfa_project/main/urls.py
@@ -3,9 +3,9 @@ from .views import *
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('', index, name='home'),
+    path('qweqweqwe', index, name='home'),
     path('about/', about, name='about'),
-    path('register/', registration, name='register'),
+    path('', registration, name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 
